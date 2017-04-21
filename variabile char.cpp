30b3776457ba31b5400dfb58ead7a8ef032fde92
giugi16 <giulia.candian0@gmail.com>
2017-04-21 22:21:41 +0200

    Add files via upload

---
+++ variabile char.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using name space std;
+int main ()
+{ char max=127;
+  char min=-128;
+  cout<<"valore minimo:"<<(int)min<<endl;
+  cout<<"valore massimo:"<<(int)max<<endl;
+  cout<<"minimo-1:"<<min-1<<endl;
+  cout<<"massimo+1:"<<max+1<<endl;
+  return 0;
+  
+
+}
