902bffb058c3a64fc38cc16d4a01bab164773aaf
giugi16 <giulia.candian0@gmail.com>
2017-04-21 23:01:34 +0200

    Add files via upload

---
+++ variabile short.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{ short max=32767;
+  short min=-32768;
+  cout<<"valore min:"<<(int)min<<endl;
+  cout<<"valore max:"<<(int)max<<endl;
+  cout<<"minimo-1"<<min-1<<endl;
+  cout<<"massimo+1"<<max+1<<endl;
+  return 0;
+}
