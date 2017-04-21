3bfbb9bb699844eb18dc3f4c5664e31e3700dd38
giugi16 <giulia.candian0@gmail.com>
2017-04-21 22:40:29 +0200

    Add files via upload

---
+++ variabile int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int max = 2147483647;
+    int min = -2147483648;
+    cout << "min: " << min << endl;
+    cout << "max: " << max << endl;
+    cout << "min-1: " << min-1 << endl;
+    cout << "max+1: " << max+1 << endl;
+    return 0;
+}
