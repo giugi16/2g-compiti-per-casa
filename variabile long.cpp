a9be0ad64b1d9f5e88112f7027051816260f9a0d
giugi16 <giulia.candian0@gmail.com>
2017-04-21 23:15:44 +0200

    Add files via upload

---
+++ variabile long.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{ long max=4294967295;
+  long min=-4294967296;
+  cout<<"min:"<<min<<endl;
+  cout<<"max:"<<max<<endl;
+  cout<<"min-1:"<<min-1<<endl;
+  cout<<"max+1:"<<max+1<<endl;
+  return 0;
+
+
+}
